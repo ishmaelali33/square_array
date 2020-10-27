adfa2a25e48c3bd84598486711cd76cf953acbb1
ishmaelali33 <ishmaelali33@gmail.com>
2020-10-27 14:38:04 -0400

    commit tree 9bd4d3c5f1b8de8c301a8f5c105c103f279f1649

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+numbers = ["1", "4", "9"]
+counter = 1
+numbers.each do |number|
+number.sqrt(1)
+number.sqrt(4)
+number.sqrt(9)
+  puts "This is loop number #{counter}"
+  puts "Square root #{number}!"
+  counter += 1
 end
